2281b9053324c6c4641b6037a786b1c2bb51393e
Oussama Elmaaroufy <slashnpro@gmail.com>
2022-02-22 17:14:32 +0100

    22-02-2022

--- resources/views/documents/show.blade.php
+++ resources/views/documents/show.blade.php
@@ -149,70 +149,73 @@
                                 Retour en arrière
                             </a>
 
-                            {!! Form::open() !!}
-                            <div class="flex">
+                            <div class="flex ml-auto">
+                                {!! Form::open() !!}
+                                <div class="flex">
+                                    @hasanyrole('Root|Admin')
+                                        @can('edit')
+                                            <a href="/documents/{{ $doc->id }}/edit"
+                                                class="rounded-full w-10 h-10 bg-bg-color p-0 border border-main inline-flex items-center justify-center text-main ml-2"
+                                                data-position="left" data-delay="50" data-tooltip="Edit this">
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 m-1" fill="none"
+                                                    viewBox="0 0 24 24" stroke="currentColor">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                        d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                                </svg>
+                                            </a>
+                                        @endcan
+                                    @endhasanyrole
+
+                                    <a href="/documents/open/{{ $doc->id }}" target="_blank"
+                                        class="rounded-full w-10 h-10 bg-bg-color p-0 border border-main inline-flex items-center justify-center text-main ml-2"
+                                        data-position="top" data-delay="50" data-tooltip="Open this">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 m-1" fill="none"
+                                            viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                        </svg>
+                                    </a>
+                                </div>
+                                {!! Form::close() !!}
+
+                                <!-- SHARE using link -->
+                                {!! Form::open(['action' => ['ShareController@update', $doc->id], 'method' => 'PATCH', 'id' => 'form-share-documents-' . $doc->id]) !!}
                                 @hasanyrole('Root|Admin')
-                                    @can('edit')
-                                        <a href="/documents/{{ $doc->id }}/edit"
-                                            class="rounded-full w-10 h-10 bg-bg-color p-0 border border-main inline-flex items-center justify-center text-main ml-2"
-                                            data-position="left" data-delay="50" data-tooltip="Edit this">
+                                    @can('shared')
+                                        <a href="#"
+                                            class="data-share rounded-full w-10 h-10 bg-bg-color p-0 border border-main inline-flex items-center justify-center text-main ml-2"
+                                            data-position="top" data-delay="50" data-tooltip="Share this"
+                                            data-form="documents-{{ $doc->id }}">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 m-1" fill="none"
                                                 viewBox="0 0 24 24" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                    d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                                    d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" />
                                             </svg>
                                         </a>
                                     @endcan
                                 @endhasanyrole
+                                {!! Form::close() !!}
 
-                                <a href="/documents/open/{{ $doc->id }}" target="_blank"
-                                    class="rounded-full w-10 h-10 bg-bg-color p-0 border border-main inline-flex items-center justify-center text-main ml-2"
-                                    data-position="top" data-delay="50" data-tooltip="Open this">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 m-1" fill="none"
-                                        viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                                    </svg>
-                                </a>
+                                <!-- DELETE using link -->
+                                {!! Form::open(['action' => ['DocumentsController@destroy', $doc->id], 'method' => 'DELETE', 'id' => 'form-delete-documents-' . $doc->id]) !!}
+                                @hasanyrole('Root|Admin')
+                                    @can('delete')
+                                        <a href="#"
+                                            class="rounded-full w-10 h-10 bg-bg-color p-0 border border-main inline-flex items-center justify-center text-main ml-2"
+                                            data-position="right" data-delay="50" data-tooltip="Delete this"
+                                            data-form="documents-{{ $doc->id }}">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 m-1" fill="none"
+                                                viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                            </svg>
+                                        </a>
+                                    @endcan
+                                @endhasanyrole
+                                {!! Form::close() !!}
                             </div>
-                            {!! Form::close() !!}
-
-                            <!-- SHARE using link -->
-                            {!! Form::open(['action' => ['ShareController@update', $doc->id], 'method' => 'PATCH', 'id' => 'form-share-documents-' . $doc->id]) !!}
-                            @hasanyrole('Root|Admin')
-                                @can('shared')
-                                    <a href="#"
-                                        class="data-share rounded-full w-10 h-10 bg-bg-color p-0 border border-main inline-flex items-center justify-center text-main ml-2"
-                                        data-position="top" data-delay="50" data-tooltip="Share this"
-                                        data-form="documents-{{ $doc->id }}">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 m-1" fill="none"
-                                            viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" />
-                                        </svg>
-                                    </a>
-                                @endcan
-                            @endhasanyrole
-                            {!! Form::close() !!}
-                            <!-- DELETE using link -->
-                            {!! Form::open(['action' => ['DocumentsController@destroy', $doc->id], 'method' => 'DELETE', 'id' => 'form-delete-documents-' . $doc->id]) !!}
-                            @hasanyrole('Root|Admin')
-                                @can('delete')
-                                    <a href="#"
-                                        class="rounded-full w-10 h-10 bg-bg-color p-0 border border-main inline-flex items-center justify-center text-main ml-2"
-                                        data-position="right" data-delay="50" data-tooltip="Delete this"
-                                        data-form="documents-{{ $doc->id }}">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 m-1" fill="none"
-                                            viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                        </svg>
-                                    </a>
-                                @endcan
-                            @endhasanyrole
-                            {!! Form::close() !!}
                         </div>
 
                     </div>
